2b3c4301a072dd76c3633a39b97d911032f07559
Dr. Alwin Simon <003alwin@gmail.com>
2023-12-10 16:36:14 +0530

    Modified User Model
    Added Lombok config.
    Modified ID type to be automatically determined.

--- src/main/java/com/alwinsimon/UserManagementJavaSpringBoot/Model/User.java
+++ src/main/java/com/alwinsimon/UserManagementJavaSpringBoot/Model/User.java
@@ -1,12 +1,18 @@
 package com.alwinsimon.UserManagementJavaSpringBoot.Model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "_user",uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
     private String firstname;
     private String lastname;
