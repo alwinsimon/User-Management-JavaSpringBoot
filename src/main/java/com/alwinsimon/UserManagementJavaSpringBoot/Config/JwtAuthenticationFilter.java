f2ef7e584f5f95f5005a319a9423c88d537db873
Dr. Alwin Simon <003alwin@gmail.com>
2023-12-10 17:44:47 +0530

    Added basic JwtAuthenticationFilter.

---
+++ src/main/java/com/alwinsimon/UserManagementJavaSpringBoot/Config/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.alwinsimon.UserManagementJavaSpringBoot.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
