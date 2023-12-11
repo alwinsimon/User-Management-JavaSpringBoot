29db4b6f608e2696b48e7b2a1a38856413eb76b0
Dr. Alwin Simon <003alwin@gmail.com>
2023-12-11 12:15:47 +0530

    Exposed Interfaces for all the Services.

--- src/main/java/com/alwinsimon/UserManagementJavaSpringBoot/Service/AdminService.java
+++ src/main/java/com/alwinsimon/UserManagementJavaSpringBoot/Service/AdminService.java
@@ -1,40 +1,12 @@
 package com.alwinsimon.UserManagementJavaSpringBoot.Service;
 
 import com.alwinsimon.UserManagementJavaSpringBoot.Model.User;
-import com.alwinsimon.UserManagementJavaSpringBoot.Repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class AdminService {
+public interface AdminService {
 
-    private final UserRepository userRepository;
+    List<User> getAllUsers();
 
-
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    public void deleteUserByEmail(String email) throws Exception {
-
-        User userToDelete = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
-
-        try {
-
-            userRepository.deleteById(userToDelete.getId());
-
-        } catch (EmptyResultDataAccessException e) {
-
-            throw new UsernameNotFoundException("User Deletion FAILED.", e);
-
-        }
-
-    }
+    void deleteUserByEmail(String email);
 
 }
--- src/main/java/com/alwinsimon/UserManagementJavaSpringBoot/Service/UserService.java
+++ src/main/java/com/alwinsimon/UserManagementJavaSpringBoot/Service/UserService.java
@@ -1,21 +1,10 @@
 package com.alwinsimon.UserManagementJavaSpringBoot.Service;
 
 import com.alwinsimon.UserManagementJavaSpringBoot.Model.User;
-import com.alwinsimon.UserManagementJavaSpringBoot.Repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.security.Principal;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
-    private final Principal principal;
+    public User currentUserDetails();
 
-    public User currentUserDetails() {
-        String email = principal.getName();
-        return userRepository.findByEmail(email).orElse(null);
-    }
 }
