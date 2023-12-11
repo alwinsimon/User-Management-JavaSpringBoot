791da9bc9e6ee1f13e39cab29744ae0cb20bba77
Dr. Alwin Simon <003alwin@gmail.com>
2023-12-11 11:07:42 +0530

    Added Admin controller and Service.

---
+++ src/main/java/com/alwinsimon/UserManagementJavaSpringBoot/Controller/AdminController.java
@@ -0,0 +1,39 @@
+package com.alwinsimon.UserManagementJavaSpringBoot.Controller;
+
+import com.alwinsimon.UserManagementJavaSpringBoot.Model.User;
+import com.alwinsimon.UserManagementJavaSpringBoot.Service.AdminService;
+import com.alwinsimon.UserManagementJavaSpringBoot.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class AdminController {
+
+    private final AdminService adminService;
+    @GetMapping("/get-users")
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        // API Endpoint to get the LoggedIn User Details using Token received in the Request Header.
+        List<User> users = adminService.getAllUsers();
+        return ResponseEntity.ok(users);
+
+    }
+
+    @DeleteMapping("/delete-user/{email}")
+    public ResponseEntity<String> deleteUser(@PathVariable("email")String email){
+        try {
+            adminService.deleteUserByEmail(email);
+            return ResponseEntity.ok("User deleted.");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User deletion Failed.");
+        }
+    }
+}
